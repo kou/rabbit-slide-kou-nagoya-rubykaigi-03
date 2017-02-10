37a33b0f7dd80e39ff46ba34d8e5945ec7c8fc7a
Kouhei Sutou <kou@clear-code.com>
2017-02-11 01:06:25 +0900

    Use panda

--- theme.rb
+++ theme.rb
@@ -1,6 +1,6 @@
 @title_slide_title_font_size = @large_font_size * 0.9
 
 # @image_slide_number_image = "hebi.svg"
-# @image_timer_image = "panda_kotatsu.svg"
+@image_timer_image = "panda_kotatsu.svg"
 
 include_theme("clear-code")
